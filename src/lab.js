b9d87e2eb2f27cc9828aa96e6c0b4b593f15cf12
Mike Bostock <mbostock@gmail.com>
2015-06-02 16:02:58 -0700

    Checkpoint RGB to Lab conversion.

--- src/lab.js
+++ src/lab.js
@@ -1,9 +1,13 @@
 import {Rgb} from "./rgb";
 
-var brighter = 18,
-    X = 0.950470, // D65 standard referent
-    Y = 1,
-    Z = 1.088830;
+var Kn = 18,
+    Xn = 0.950470, // D65 standard referent
+    Yn = 1,
+    Zn = 1.088830,
+    t0 = 4 / 29,
+    t1 = 6 / 29,
+    t2 = 3 * t1 * t1,
+    t3 = t1 * t1 * t1;
 
 export function Lab(l, a, b) {
   this.l = Math.max(0, Math.min(100, +l));
@@ -17,20 +21,20 @@ function lab(l, a, b) {
 
 Lab.prototype = lab.prototype = {
   brighter: function(k) {
-    return new Lab(this.l + brighter * (k == null ? 1 : k), this.a, this.b);
+    return new Lab(this.l + Kn * (k == null ? 1 : k), this.a, this.b);
   },
   darker: function(k) {
-    return new Lab(this.l - brighter * (k == null ? 1 : k), this.a, this.b);
+    return new Lab(this.l - Kn * (k == null ? 1 : k), this.a, this.b);
   },
   rgb: function() {
     var y = (this.l + 16) / 116,
         x = y + this.a / 500,
         z = y - this.b / 200;
-    x = lab2xyz(x) * X;
-    y = lab2xyz(y) * Y;
-    z = lab2xyz(z) * Z;
+    y = Yn * lab2xyz(y);
+    x = Xn * lab2xyz(x);
+    z = Zn * lab2xyz(z);
     return new Rgb(
-      xyz2rgb( 3.2404542 * x - 1.5371385 * y - 0.4985314 * z),
+      xyz2rgb( 3.2404542 * x - 1.5371385 * y - 0.4985314 * z), // D65 -> sRGB
       xyz2rgb(-0.9692660 * x + 1.8760108 * y + 0.0415560 * z),
       xyz2rgb( 0.0556434 * x - 0.2040259 * y + 1.0572252 * z)
     );
@@ -40,12 +44,20 @@ Lab.prototype = lab.prototype = {
   }
 };
 
-function lab2xyz(x) {
-  return x > 0.206893034 ? x * x * x : (x - 4 / 29) / 7.787037;
+function xyz2lab(t) {
+  return t > t3 ? Math.pow(t, 1 / 3) : t / t2 + t0;
 }
 
-function xyz2rgb(r) {
-  return 255 * (r <= 0.00304 ? 12.92 * r : 1.055 * Math.pow(r, 1 / 2.4) - 0.055);
+function lab2xyz(t) {
+  return t > t1 ? t * t * t : t2 * (t - t0);
+}
+
+function xyz2rgb(x) {
+  return 255 * (x <= 0.0031308 ? 12.92 * x : 1.055 * Math.pow(x, 1 / 2.4) - 0.055);
+}
+
+function rgb2xyz(x) {
+  return (x /= 255) <= 0.04045 ? x / 12.92 : Math.pow((x + 0.055) / 1.055, 2.4);
 }
 
 export default lab;

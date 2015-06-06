74c1e704e525f396dee48eaa901a1fcab6417fdf
Mike Bostock <mbostock@gmail.com>
2015-06-05 21:23:23 -0700

    Cubehelix colorspace!
    RGB-to-Cubehelix conversion courtesy of @jasondavies.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import {default as rgb, Rgb} from "./src/rgb";
 import {default as hsl, Hsl} from "./src/hsl";
 import {default as lab, Lab} from "./src/lab";
 import {default as hcl, Hcl} from "./src/hcl";
+import {default as cubehelix, Cubehelix} from "./src/cubehelix";
 import interpolateRgb from "./src/interpolateRgb";
 import interpolateHsl from "./src/interpolateHsl";
 import interpolateLab from "./src/interpolateLab";
 import interpolateHcl from "./src/interpolateHcl";
+import interpolateCubehelix from "./src/interpolateCubehelix";
 
 // Done lazily to avoid circular dependency between Color, Rgb and Hsl.
 color.prototype = Color.prototype;
@@ -14,6 +16,7 @@ rgb.prototype = Rgb.prototype;
 hsl.prototype = Hsl.prototype;
 lab.prototype = Lab.prototype;
 hcl.prototype = Hcl.prototype;
+cubehelix.prototype = Cubehelix.prototype;
 
 export {
   color,
@@ -21,8 +24,10 @@ export {
   hsl,
   lab,
   hcl,
+  cubehelix,
   interpolateRgb,
   interpolateHsl,
   interpolateLab,
-  interpolateHcl
+  interpolateHcl,
+  interpolateCubehelix
 };
---
+++ src/cubehelix.js
@@ -0,0 +1,63 @@
+import {default as color, Color} from "./color";
+import {default as rgb, Rgb} from "./rgb";
+import {deg2rad, rad2deg} from "./hcl";
+
+var gamma = 1, // Default gamma. TODO Customize.
+    A = -0.14861,
+    B = +1.78277,
+    C = -0.29227,
+    D = -0.90649,
+    E = +1.97294,
+    ED = E * D,
+    EB = E * B,
+    BC_DA = B * C - D * A;
+
+export default function(h, s, l) {
+  if (arguments.length === 1) {
+    if (h instanceof Cubehelix) {
+      l = h.l;
+      s = h.s;
+      h = h.h;
+    } else {
+      if (!(h instanceof Rgb)) h = rgb(h);
+      var r = h.r / 255, g = h.g / 255, b = h.b / 255;
+      l = (BC_DA * b + ED * r - EB * g) / (BC_DA + ED - EB);
+      var bl = b - l, k = (E * (g - l) - C * bl) / D, lgamma = Math.pow(l, gamma);
+      s = Math.sqrt(k * k + bl * bl) / (E * lgamma * (1 - lgamma)); // NaN if lgamma=0 or lgamma=1
+      h = s ? Math.atan2(k, bl) * rad2deg : NaN;
+      if (h < 0) h += 360;
+    }
+  }
+  return new Cubehelix(h, s, l);
+};
+
+export function Cubehelix(h, s, l) {
+  this.h = +h;
+  this.s = +s;
+  this.l = +l;
+};
+
+var prototype = Cubehelix.prototype = new Color;
+
+prototype.brighter = function(k) {
+  k = k == null ? brighter : Math.pow(brighter, k);
+  return new Cubehelix(this.h, this.s, this.l * k);
+};
+
+prototype.darker = function(k) {
+  k = k == null ? darker : Math.pow(darker, k);
+  return new Cubehelix(this.h, this.s, this.l * k);
+};
+
+prototype.rgb = function() {
+  var h = this.h ? this.h * deg2rad : 0,
+      l = Math.pow(this.l, gamma),
+      a = this.s ? this.s * l * (1 - l) : 0,
+      cosh = Math.cos(h),
+      sinh = Math.sin(h);
+  return new Rgb(
+    255 * (l + a * (A * cosh + B * sinh)),
+    255 * (l + a * (C * cosh + D * sinh)),
+    255 * (l + a * (E * cosh))
+  );
+};

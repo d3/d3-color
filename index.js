ee6074313401660047dd913e02471a455730eb2f
Mike Bostock <mbostock@gmail.com>
2015-11-04 13:19:16 -0800

    Concise exports.

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-import color from "./src/color";
-import rgb from "./src/rgb";
-import hsl from "./src/hsl";
-import lab from "./src/lab";
-import hcl from "./src/hcl";
-import cubehelix from "./src/cubehelix";
-import interpolateRgb from "./src/interpolateRgb";
-import interpolateHsl from "./src/interpolateHsl";
-import interpolateHslLong from "./src/interpolateHslLong";
-import interpolateLab from "./src/interpolateLab";
-import interpolateHcl from "./src/interpolateHcl";
-import interpolateHclLong from "./src/interpolateHclLong";
-import interpolateCubehelixGamma from "./src/interpolateCubehelixGamma";
-import interpolateCubehelixGammaLong from "./src/interpolateCubehelixGammaLong";
+export {default as color} from "./src/color";
+export {default as rgb} from "./src/rgb";
+export {default as hsl} from "./src/hsl";
+export {default as lab} from "./src/lab";
+export {default as hcl} from "./src/hcl";
+export {default as cubehelix} from "./src/cubehelix";
+export {default as interpolateRgb} from "./src/interpolateRgb";
+export {default as interpolateHsl} from "./src/interpolateHsl";
+export {default as interpolateHslLong} from "./src/interpolateHslLong";
+export {default as interpolateLab} from "./src/interpolateLab";
+export {default as interpolateHcl} from "./src/interpolateHcl";
+export {default as interpolateHclLong} from "./src/interpolateHclLong";
+export {default as interpolateCubehelixGamma} from "./src/interpolateCubehelixGamma";
+export {default as interpolateCubehelixGammaLong} from "./src/interpolateCubehelixGammaLong";
 
 export var interpolateCubehelix = interpolateCubehelixGamma(1);
 export var interpolateCubehelixLong = interpolateCubehelixGammaLong(1);
-
-export {
-  color,
-  rgb,
-  hsl,
-  lab,
-  hcl,
-  cubehelix,
-  interpolateRgb,
-  interpolateHsl,
-  interpolateHslLong,
-  interpolateLab,
-  interpolateHcl,
-  interpolateHclLong,
-  interpolateCubehelixGamma,
-  interpolateCubehelixGammaLong
-};

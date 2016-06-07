e82548aa5fbca2342bb62f3befb228f19b3b1009
Mike Bostock <mbostock@gmail.com>
2016-06-07 16:23:59 -0700

    Export to d3.
    Related d3/d3#2840.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-export {version} from "./build/package";
 export {default as color, rgb, hsl} from "./src/color";
 export {default as lab, hcl} from "./src/lab";
 export {default as cubehelix} from "./src/cubehelix";

c1a5c40a89ab81b8322e4d4a7e817635c7c71e57
acterhd <acterhd@gmail.com>
2016-07-01 23:07:04 +0800

    reformat for HCG only

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-export {default as color, rgb, hsl} from "./src/color";
-export {default as lab, hcl} from "./src/lab";
-export {default as cubehelix} from "./src/cubehelix";
 export {default as hcg} from "./src/hcg";
--- test/rgbEqual.js
+++ test/rgbEqual.js
@@ -1,8 +1,8 @@
 var tape = require("tape"),
-    color = require("../");
+    d3_color = require("d3-color");
 
 tape.Test.prototype.rgbStrictEqual = function(actual, r, g, b, opacity) {
-  this._assert(actual instanceof color.rgb
+  this._assert(actual instanceof d3_color.rgb
       && (isNaN(r) ? isNaN(actual.r) && actual.r !== actual.r : actual.r === r)
       && (isNaN(g) ? isNaN(actual.g) && actual.g !== actual.g : actual.g === g)
       && (isNaN(b) ? isNaN(actual.b) && actual.b !== actual.b : actual.b === b)
@@ -15,7 +15,7 @@ tape.Test.prototype.rgbStrictEqual = function(actual, r, g, b, opacity) {
 };
 
 tape.Test.prototype.rgbEqual = function(actual, r, g, b, opacity) {
-  this._assert(actual instanceof color.rgb
+  this._assert(actual instanceof d3_color.rgb
       && (isNaN(r) ? isNaN(actual.r) && actual.r !== actual.r : Math.round(actual.r) === Math.round(r))
       && (isNaN(g) ? isNaN(actual.g) && actual.g !== actual.g : Math.round(actual.g) === Math.round(g))
       && (isNaN(b) ? isNaN(actual.b) && actual.b !== actual.b : Math.round(actual.b) === Math.round(b))

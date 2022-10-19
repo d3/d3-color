a18c117d996ffceefe4b6a9eef7072fea19b5ce4
Brady Conlon <brady.conlon.int@paperlessparts.com>
2022-10-19 15:03:37 -0400

    added support for esm

--- rollup.config.js
+++ rollup.config.js
@@ -21,6 +21,17 @@ const config = {
     banner: `// ${meta.homepage} v${meta.version} Copyright ${copyright}`,
     globals: Object.assign({}, ...Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)).map(key => ({[key]: "d3"})))
   },
+  input: "src/index.js",
+  external: Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)),
+  output: {
+    file: `dist/${meta.name}.mjs`,
+    name: "d3",
+    format: "esm",
+    indent: false,
+    extend: true,
+    banner: `// ${meta.homepage} v${meta.version} Copyright ${copyright}`,
+    globals: Object.assign({}, ...Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)).map(key => ({[key]: "d3"})))
+  },
   plugins: []
 };
 
